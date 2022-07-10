f58ec8572d5b8aa49077f76c1f47ee1dd8496489
jonas.svensson <jonas.svensson@midsummer.se>
2022-07-10 10:38:47 +0200

    React-create, bootstrap, Card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import {Row, Col} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Container>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Header>Simulation</Card.Header>
+              <Card.Body>
+                <Card.Title>Special title treatment</Card.Title>
+                <Card.Text>
+                  With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              </Card.Body>          
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Header>Orientation from Raspberry Pi</Card.Header>
+              <Card.Body>
+                <Card.Title>Special title treatment</Card.Title>
+                <Card.Text>
+                  With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              </Card.Body>          
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
